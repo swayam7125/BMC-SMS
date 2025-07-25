3e781ecb9b74c2b10d859299a917d8bae98d3cb2
swayam7125 <otherswayam@gmail.com>
2025-07-25 15:11:23 +0530

    edited past principal

--- pages/past_record/past_principal.php
+++ pages/past_record/past_principal.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once "../../includes/connect.php";
 include_once "../../encryption.php";
 
@@ -14,7 +13,7 @@ if (!$role) {
     exit;
 }
 
-// Fetch deleted principal data with school name
+// Fetch deleted principal data and join with the school table to get the school_name
 $query = "SELECT dp.*, s.school_name 
           FROM deleted_principals dp
           LEFT JOIN school s ON dp.school_id = s.id
@@ -24,33 +23,24 @@ $result = mysqli_query($conn, $query);
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Deleted Principal Records - School Management System</title>
-
     <link href="../../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="../../assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <link href="../../assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
-
 <body id="page-top">
     <div id="wrapper">
-
         <?php include_once '../../includes/sidebar.php'; ?>
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
-
                 <?php include_once '../../includes/header.php'; ?>
-
                 <div class="container-fluid">
                     <h1 class="h3 mb-2 text-gray-800">Deleted Principal Records</h1>
                     <p class="mb-4">A log of all principals that have been deleted from the system.</p>
-
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
                             <h6 class="m-0 font-weight-bold text-primary">Deleted Principals Log</h6>
@@ -64,13 +54,8 @@ $result = mysqli_query($conn, $query);
                                             <th>Name</th>
                                             <th>Email</th>
                                             <th>Phone</th>
-                                            <th>DOB</th>
-                                            <th>Gender</th>
-                                            <th>Blood Group</th>
-                                            <th>Address</th>
-                                            <th>Qualification</th>
-                                            <th>Salary</th>
-                                            <th>School ID</th>
+                                            <th>School Name</th>
+                                            <th>Batch</th>
                                             <th>Deleted By</th>
                                             <th>Deleted At</th>
                                         </tr>
@@ -84,19 +69,14 @@ $result = mysqli_query($conn, $query);
                                                 echo "<td>" . htmlspecialchars($row['principal_name'] ?? 'N/A') . "</td>";
                                                 echo "<td>" . htmlspecialchars($row['email'] ?? 'N/A') . "</td>";
                                                 echo "<td>" . htmlspecialchars($row['phone'] ?? 'N/A') . "</td>";
-                                                echo "<td>" . htmlspecialchars($row['dob'] ?? 'N/A') . "</td>";
-                                                echo "<td>" . htmlspecialchars($row['gender'] ?? 'N/A') . "</td>";
-                                                echo "<td>" . htmlspecialchars($row['blood_group'] ?? 'N/A') . "</td>";
-                                                echo "<td>" . htmlspecialchars($row['address'] ?? 'N/A') . "</td>";
-                                                echo "<td>" . htmlspecialchars($row['qualification'] ?? 'N/A') . "</td>";
-                                                echo "<td>" . htmlspecialchars($row['salary'] ?? 'N/A') . "</td>";
-                                                echo "<td>" . htmlspecialchars($row['school_id'] ?? 'N/A') . "</td>";
+                                                echo "<td>" . htmlspecialchars($row['school_name'] ?? 'N/A') . "</td>"; // Display School Name
+                                                echo "<td>" . htmlspecialchars($row['batch'] ?? 'N/A') . "</td>";
                                                 echo "<td>" . htmlspecialchars($row['deleted_by_role'] ?? 'N/A') . "</td>";
                                                 echo "<td>" . htmlspecialchars($row['deleted_at'] ?? 'N/A') . "</td>";
                                                 echo "</tr>";
                                             }
                                         } else {
-                                            echo "<tr><td colspan='13' class='text-center'>No deleted principals found</td></tr>";
+                                            echo "<tr><td colspan='8' class='text-center'>No deleted principals found</td></tr>";
                                         }
                                         ?>
                                     </tbody>
@@ -106,23 +86,16 @@ $result = mysqli_query($conn, $query);
                     </div>
                 </div>
             </div>
-
             <?php include '../../includes/footer.php'; ?>
         </div>
     </div>
-
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
+    <a class="scroll-to-top rounded" href="#page-top"><i class="fas fa-angle-up"></i></a>
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
                 </div>
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
@@ -132,25 +105,19 @@ $result = mysqli_query($conn, $query);
             </div>
         </div>
     </div>
-
     <script src="../../assets/vendor/jquery/jquery.min.js"></script>
     <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
     <script src="../../assets/js/sb-admin-2.min.js"></script>
     <script src="../../assets/vendor/datatables/jquery.dataTables.min.js"></script>
     <script src="../../assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
     <script>
         $(document).ready(function() {
             $('#dataTable').DataTable({
                 "pageLength": 10,
-                "order": [
-                    [12, "desc"] // Sort by the "Deleted At" column, which is the 13th column (index 12)
-                ]
+                "order": [[7, "desc"]] // Sort by the "Deleted At" column (index 7)
             });
         });
     </script>
-
 </body>
-
 </html>
