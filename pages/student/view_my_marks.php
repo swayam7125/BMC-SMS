8f8e7b144a396c3337effdd564f5859104df6c6e
devam1135 <devamparekh120@gmail.com>
2025-07-25 15:29:19 +0530

    Add Font Awesome stylesheet link for improved icon support

--- pages/student/view_my_marks.php
+++ pages/student/view_my_marks.php
@@ -36,6 +36,7 @@ $academic_year_suggestion = $current_year . '-' . ($current_year + 1);
     <meta charset="utf-8">
     <title>My Marks Report - School Management System</title>
     <link href="../../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,900" rel="stylesheet">
     <link href="../../assets/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
