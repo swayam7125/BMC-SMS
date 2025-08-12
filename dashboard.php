2f1905a9ce6d14bf116e25a93d39f72392e2bfc5
codebyharsh <harshshah2601@gmail.com>
2025-08-12 17:55:04 +0530

    clean up code

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,4 @@
 <?php
-// Enable all error reporting for debugging
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 include_once "encryption.php";
 include_once "./includes/connect.php"; // Uses the new PDO $conn object
 
--- includes/chart_data/dynamic_chart_data.php
+++ includes/chart_data/dynamic_chart_data.php
@@ -1,8 +1,5 @@
 <?php
 // --- Centralized Chart Data Provider ---
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 header('Content-Type: application/json');
 
 // Include the connection file
