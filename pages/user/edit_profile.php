4d187199101167f8222ede4bad27d6df30d6614a
Fenil178 <fenilpastagia1708@gmail.com>
2025-08-26 21:58:59 +0530

    Add CDN once again

--- pages/user/edit_profile.php
+++ pages/user/edit_profile.php
@@ -235,6 +235,7 @@ if (isset($_COOKIE['encrypted_user_id']) && isset($_COOKIE['encrypted_user_role'
     <link href="../../assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <link rel="stylesheet" href="../../assets/css/sidebar.css">
+    <link rel="stylesheet" href="../../assets/css/scrollbar_hidden.css">
 </head>
 
 <body id="page-top">
