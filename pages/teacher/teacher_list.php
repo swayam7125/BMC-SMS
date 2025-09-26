b6eee33e99398c5779d51f62980c3d7c09cfd690
Fenil178 <fenilpastagia1708@gmail.com>
2025-09-26 17:33:42 +0530

    remove script

--- pages/teacher/teacher_list.php
+++ pages/teacher/teacher_list.php
@@ -246,7 +246,6 @@ if (!is_ajax_request()) {
     <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
     <script src="../../assets/js/sb-admin-2.min.js"></script>
-    <script src="../../assets/vendor/datatables/jquery.dataTables.min.js"></script>
     <script src="../../assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
     <script>
     $(document).ready(function() {
@@ -267,7 +266,7 @@ if (!is_ajax_request()) {
         $('#confirmDeleteBtn').attr('href', deleteUrl);
         $('#deleteModal').modal('show');
     }
-</script>
+    </script>
 </body>
 <?php
 // Add this block at the very end of the file
