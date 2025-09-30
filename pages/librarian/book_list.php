99935c25bb9cfd5f7f60501a69a3b399417aec2b
Fenil178 <fenilpastagia1708@gmail.com>
2025-09-30 15:41:02 +0530

    add CDN

--- pages/librarian/book_list.php
+++ pages/librarian/book_list.php
@@ -35,6 +35,11 @@ try {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <link rel="stylesheet" href="../../assets/css/sidebar.css">
     <link rel="stylesheet" href="../../assets/css/scrollbar_hidden.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700" rel="stylesheet">
+    <link href="../../assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../assets/css/table-to-card.css">
+    <link rel="stylesheet" href="../../assets/css/responsive.css" />
 </head>
 <body id="page-top">
     <div id="wrapper">
